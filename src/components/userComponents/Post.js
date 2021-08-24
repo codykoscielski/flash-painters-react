7d1327b54d1675b96a373a2953355dc3fa2baef2
dev-cody <60411570+dev-cody@users.noreply.github.com>
2021-08-23 21:44:02 -0400

    started to style the post component and setup useRef.

--- src/components/userComponents/Post.js
+++ src/components/userComponents/Post.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
+import { Alert, Button } from 'react-bootstrap';
 
 const Post = () => {
+
+    const priceRef = useRef()
+    const titleRef = useRef()
+    const descRef = useRef()
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState('')
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+    }
+    
     return (
-        <div>
-            <h1>Creating the post form</h1>
+        <div className="wrapper">
+            {error && <Alert variant="danger">{ error }</Alert>}
+            <div className="form-container">
+                <form className="form" id="createAccountForm" onSubmit={handleSubmit}>
+                    <h1 className="form-title">Create Listing</h1>
+                    <div className="input-group">
+                        <input type="file" className="form-input" required/>
+                    </div>
+                    <div className="input-group">
+                        <input type="text" className="form-input"  placeholder="Title"  ref={ titleRef } required/>
+                    </div>
+                    <div className="input-group">
+                        <input type="number" className="form-input"  placeholder="price in US" ref={ priceRef } required/>
+                    </div>
+                    <div className="input-group">
+                        <textarea className="form-input" placeholder="Description" ref={ descRef } />
+                    </div>
+                    <Button className="btn form-btn" type="submit" disabled={ loading }>Post</Button> 
+                </form>
+            </div>
         </div>
     )
 }
--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -13,7 +13,7 @@ export const AuthProvider = ({ children }) => {
     const [loading, setLoading] = useState(true);
 
     const signup = (email, password) => {
-        return auth.createUserWithEmailAndPassword(email, password)
+        return auth.createUserWithEmailAndPassword(email, password)       
     }
 
     const login = (email, password) => {
