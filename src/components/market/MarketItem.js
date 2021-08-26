ba37f0025cb90c34cda29f5f6c353cc7e0e1a258
dev-cody <60411570+dev-cody@users.noreply.github.com>
2021-08-26 18:17:12 -0400

    Corrected an error that stopped market cards from rendering.

--- src/components/market/MarketItem.js
+++ src/components/market/MarketItem.js
@@ -12,7 +12,6 @@ const MarketItem = ({painting: { title, artistName, price }}) => {
             <Card.Body>
                 <Card.Img src="https://firebasestorage.googleapis.com/v0/b/flash-painters.appspot.com/o/IMG_1623.JPG?alt=media&token=4739b253-5182-4dca-99bb-5aa36fceb30e"/>
                 <Card.Title>{ title }</Card.Title>
-                <Card.Subtitle>{ currentUser.displayName }</Card.Subtitle>
                 <Card.Text>${ price }</Card.Text>
                 <Button variant="primary">Buy</Button>
             </Card.Body>
