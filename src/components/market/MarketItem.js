edfce28022e088b3659ca6576827a9c7d583986a
dev-cody <60411570+dev-cody@users.noreply.github.com>
2021-09-13 19:37:07 -0400

    Restyled the market cards, still needs some work

--- src/components/market/MarketItem.js
+++ src/components/market/MarketItem.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 
 const MarketItem = ({painting: { title, imageURL, price, artistName }}) => {
 
     return (
-        <Card style={{ width: '18rem' }}>
-            <Card.Body>
-                <Card.Img src={ imageURL }/>
-                <Card.Title>{ title }</Card.Title>
-                <Card.Subtitle>{ artistName }</Card.Subtitle>
-                <Card.Text>${ price }</Card.Text>
-                <Button variant="primary">Buy</Button>
-            </Card.Body>
-        </Card>
+        <div className='card-box'>
+            <img src={ imageURL } alt={ title } />
+            <div className="card-body">
+                <div className="card-text">
+                    <h4>{ title }</h4>
+                    <p>{ artistName }</p>
+                </div>
+                <div className="card-price">
+                    <h4>${ price }</h4>
+                </div>
+            </div>
+        </div>
     )
 }
 
